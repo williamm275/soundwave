7dd6b0fa3e17a86b20859fec8a5a74cd27d08561
William Martinez <wrmartinez709@gmail.com>
2023-01-24 20:19:06 -0500

    Update redirect URI url

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = "f6b73961837c4b3ab4aec15b928dedcf";
-const redirectURI = "http://localhost:8888/callback";
+const redirectURI = "https://soundwave.pages.dev/";
 let accessToken = "";
 
 const Spotify = {
