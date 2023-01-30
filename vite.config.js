ef7b4593ef5bc61baf36fb69b8ddf268bb8d6a3f
William Martinez <wrmartinez709@gmail.com>
2023-01-30 05:10:40 -0500

    Fix API key issue

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
 
-const viteEnv = {};
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
-  }
-});
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "@": require("path").resolve(__dirname, "src"),
-  },
-  define: viteEnv,
   plugins: [react()],
 });
