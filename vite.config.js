e0332038737605f1e0a96b5a7ea751cb97e812b2
William Martinez <wrmartinez709@gmail.com>
2023-01-30 04:59:01 -0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
 });
+
+export default {
+  alias: {
+    "@": require("path").resolve(__dirname, "src"),
+  },
+  define: viteEnv,
+  plugins: [vue()],
+};
