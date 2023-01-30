53837d640f1f281ed9707cc3a07c4094cbc97bd0
William Martinez <wrmartinez709@gmail.com>
2023-01-30 05:19:53 -0500

    Fix API bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  vite: {
+    define: {
+      "process.env.VITE_SHAZAM_CORE_RAPID_API_KEY": JSON.stringify(
+        process.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      ),
+      "process.env.VITE_GEO_API_KEY": JSON.stringify(
+        process.env.VITE_GEO_API_KEY
+      ),
+    },
+  },
   plugins: [react()],
 });
